730da9cbba9ace18511b8ff812a6fd7223b4293b
Connor Brezinsky <connor.brezinsky@gmail.com>
2016-11-19 16:12:03 -0600

    maven

--- Gui/src/com/connorbrezinsky/msg/gui/main/Main.java
+++ Gui/src/com/connorbrezinsky/msg/gui/main/Main.java
@@ -1,12 +1,5 @@
 package com.connorbrezinsky.msg.gui.main;
 
-
-import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
-import javafx.stage.Stage;
-
 public class Main extends Application {
 	  public static void main(String[] args) {
 	        Application.launch(Main.class, args);
